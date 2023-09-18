f71342159ccce5317043089bba0d647d868e2e27
André Sousa <andre.castro.sousa@gmail.com>
2023-09-18 11:19:21 +0000

    add: fail tests to fail build

--- src/test/java/com/example/dockertest/DockerTestApplicationTests.java
+++ src/test/java/com/example/dockertest/DockerTestApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.dockertest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DockerTestApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1, 3);
     }
 
 }
